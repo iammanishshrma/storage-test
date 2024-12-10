9024c96dc21f627a32ab1c499d714c0f31c1da8d
Manish Sharma <manish.sharma@pelocal.com>
2024-12-10 19:13:21 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>localstorate token: {localStorage.getItem("token")}</h1>
+      <h1>sessionStorage token: {sessionStorage.getItem("token")}</h1>
+    </div>
+  );
+};
+
+export default App;
